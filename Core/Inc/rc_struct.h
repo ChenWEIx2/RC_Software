e680a815e690a61dcde048f4734b1eef718d6ace
ChenWEIx2 <1374691864@qq.com>
2021-06-09 02:20:41 +0800

    Build projet

---
+++ Core/Inc/rc_struct.h
@@ -0,0 +1,26 @@
+/**
+  ******************************************************************************
+  * @file           : rc_struct.h
+  * @brief          : This is a struct,which include all rc datas.
+  ******************************************************************************
+*/
+
+#ifndef __RC_STRUCT_H__
+#define __RC_STRUCT_H__
+
+typedef struct 
+{
+    double ch1_x;
+    double ch1_y;
+    double ch2_x;
+    double ch2_y;
+
+    bool key_0;
+    bool key_1;
+    bool key_2;
+    bool key_3;
+    
+}__rc_struct;
+
+
+#endif
