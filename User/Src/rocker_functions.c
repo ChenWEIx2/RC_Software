ae81c2776cca6b904627d07d43ec58ca614ec631
ChenWEIx2 <1374691864@qq.com>
2021-06-28 21:38:40 +0800

    Add Rcoker_Data_Limit

--- User/Src/rocker_functions.c
+++ User/Src/rocker_functions.c
@@ -18,4 +18,22 @@ void Rocker_Data_ADC2Control(__Control_Data* control_data,volatile uint16_t* adc
     control_data->roll     = 2000 - (uint16_t)(0.25f * adc_data[1]) - offset_roll;
     control_data->throttle = 2000 - (uint16_t)(0.25f * adc_data[2]) - offset_throttle;
     control_data->yaw      = 1000 + (uint16_t)(0.25f * adc_data[3]) - offset_yaw;
+
+    Rocker_Data_Limit(control_data);
+}
+
+
+void Rocker_Data_Limit(__Control_Data* control_data)
+{
+    control_data->yaw = (control_data->yaw <= 1000)? 1000:control_data->yaw;
+    control_data->yaw = (control_data->yaw >= 2000)? 2000:control_data->yaw;
+
+    control_data->roll = (control_data->roll <= 1000)? 1000:control_data->roll;
+    control_data->roll = (control_data->roll >= 2000)? 2000:control_data->roll;
+    control_data->pitch = (control_data->pitch <= 1000)? 1000:control_data->pitch;
+    control_data->pitch = (control_data->pitch >= 2000)? 2000:control_data->pitch;
+
+    control_data->throttle = (control_data->throttle <= 1000)? 1000:control_data->throttle;
+    control_data->throttle = (control_data->throttle >= 2000)? 2000:control_data->throttle;
+
 }
